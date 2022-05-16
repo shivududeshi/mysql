e33bf2274da755e457b4eb32ad999c3f738a1fec
shivududeshi <shivudu.deshi@mobigesture.com>
2022-05-16 20:21:24 +0530

    add_to_cart was started

--- item_categories.py
+++ item_categories.py
@@ -1,9 +1,6 @@
 import requests
-# import boto3
 import datetime
-import srp
 import json
-# from pycognito import Cognito
 import hmac
 import hashlib
 import base64
@@ -87,19 +84,36 @@ class egoclient:
         print(r.status_code)
         print(r.json())
     
+    def add_cart(self,access_token):
+        self.url = "https://m76jgm5mv5a5ta56kwht6e6ipm.appsync-api.us-east-1.amazonaws.com/graphql"
+        self.headers = {
+                'authorization': access_token,
+                'Content-Type': 'application/json'
+                }
+        self.payload = "query":"mutation($input:CreateCartInputu0021){n createCart(input: $input) {n idn customer_idn }n }"
+        r = requests.post(url=self.url,data=self.payload,headers=self.headers)
+        print(r.status_code)
+        print(r.json())
+    
 
     def login_with_srp(self,username= None,password=None):
         
         #get access token using USER_SRP_AUTH
 
+        pool_id='us-east-1_e5uzGdrC6'
+        client_id='2m2s4a2m2cn62pb6jfhrarqju1'
+        pool_region='us-east-1'
+
         aws = aws_srp.AWSSRP(
         username=username,
         password=password,
-        pool_id='us-east-1_e5uzGdrC6',
-        client_id='2m2s4a2m2cn62pb6jfhrarqju1',
-        pool_region='us-east-1'
+        pool_id=pool_id,
+        client_id=client_id,
+        pool_region=pool_region
         )
-        srp_a_hex =aws.get_auth_params()['SRP_A']
+        srp_a =aws.get_auth_params()['SRP_A']
+        print(aws.get_auth_params())
+        print(help(aws))
 
         self.url = "https://cognito-idp.us-east-1.amazonaws.com/"
         self.headers={
@@ -108,10 +122,10 @@ class egoclient:
         }
         payload ={
             "AuthFlow":"USER_SRP_AUTH",
-            "ClientId":"2m2s4a2m2cn62pb6jfhrarqju1",
+            "ClientId":client_id,
             "AuthParameters":{
             "USERNAME":username,
-            "SRP_A":srp_a_hex
+            "SRP_A":srp_a
             }
             }
         self.payload=json.dumps(payload)
@@ -125,19 +139,14 @@ class egoclient:
         secret_key=res1.json()['ChallengeParameters']['SECRET_BLOCK']
         user_name=res1.json()['ChallengeParameters']['USERNAME']
         time_stamp=datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y")
-        user_pool_id='us-east-1_e5uzGdrC6'
 
-        user_id_for_srp=res1.json()['ChallengeParameters']['USER_ID_FOR_SRP']
 
         secret_key_bytes = base64.standard_b64decode(secret_key)
-        msg = bytearray(user_pool_id.split('_')[1], 'utf-8') + bytearray(user_id_for_srp, 'utf-8') + \
+        msg = bytearray(pool_id.split('_')[1], 'utf-8') + bytearray(user_id_for_srp, 'utf-8') + \
             bytearray(secret_key_bytes) + bytearray(time_stamp, 'utf-8')
-        
         hkdf = aws.get_password_authentication_key(user_id_for_srp,password,srp_b, salt)
         hmac_obj = hmac.new(hkdf, msg, digestmod=hashlib.sha256)
-        signature_string = base64.standard_b64encode(hmac_obj.digest())
-
-
+        signature_string = (base64.standard_b64encode(hmac_obj.digest())).decode()
         self.url = "https://cognito-idp.us-east-1.amazonaws.com/"
         self.headers={
         "content-type" : "application/x-amz-json-1.1",
@@ -145,12 +154,12 @@ class egoclient:
         }
         payload ={
             "ChallengeName":"PASSWORD_VERIFIER",
-            "ClientId":"2m2s4a2m2cn62pb6jfhrarqju1",
+            "ClientId":client_id,
             "ChallengeResponses":{
             "USERNAME":user_name,
             "PASSWORD_CLAIM_SECRET_BLOCK":secret_key,
             "TIMESTAMP":time_stamp,
-            "PASSWORD_CLAIM_SIGNATURE":signature_string.decode('utf-8')
+            "PASSWORD_CLAIM_SIGNATURE":signature_string
             }
             }
         self.payload=json.dumps(payload)
@@ -159,28 +168,91 @@ class egoclient:
         print('call 2:',res2.status_code)
         return res2.json()['AuthenticationResult']['AccessToken']
 
-        # srp_user = srp.User(username,password)
-        # a,srp_a_bytes = srp_user.start_authentication()
-        # srp_a_hex =srp_a_bytes.hex()
-        # raw_sign_input='e5uzGdrC6'+user_id_for_srp+secret_key+time_stamp
-        # print('raw sign input',raw_sign_input)
-        # key=bytes(raw_sign_input,'UTF-8')
-        # print('raw sign input buytes',key)
-        # hmac_sign = hmac.new(key, digestmod=hashlib.sha256).digest()
-        # # print(hmac_sign)
-        # signature_key=base64.b64encode(hmac_sign).decode()
+
+    
+    def user_login_with_srp(self,username= None,password=None):
+        
+        #get access token using USER_SRP_AUTH
+
+        pool_id='us-east-1_LmIBVgrWX'
+        client_id='1elqc1ok4eqb1c9sjlhhiq74sd'
+        pool_region='us-east-1'
+
+        aws = aws_srp.AWSSRP(
+        username=username,
+        password=password,
+        pool_id=pool_id,
+        client_id=client_id,
+        pool_region=pool_region
+        )
+        srp_a =aws.get_auth_params()['SRP_A']
+
+        self.url = "https://cognito-idp.us-east-1.amazonaws.com/"
+        self.headers={
+        "content-type" : "application/x-amz-json-1.1",
+        "x-amz-target" : "AWSCognitoIdentityProviderService.InitiateAuth"
+        }
+        payload ={
+            "AuthFlow":"USER_SRP_AUTH",
+            "ClientId":client_id,
+            "AuthParameters":{
+            "USERNAME":username,
+            "SRP_A":srp_a
+            }
+            }
+        self.payload=json.dumps(payload)
+
+        res1 = requests.post(url=self.url, headers=self.headers, data=self.payload)
+        print('call 1:',res1.status_code)
+
+        srp_b=int(res1.json()['ChallengeParameters']['SRP_B'],16)
+        salt=res1.json()['ChallengeParameters']['SALT']
+        user_id_for_srp=res1.json()['ChallengeParameters']['USER_ID_FOR_SRP']
+        secret_key=res1.json()['ChallengeParameters']['SECRET_BLOCK']
+        user_name=res1.json()['ChallengeParameters']['USERNAME']
+        time_stamp=datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y")
+
+
+        secret_key_bytes = base64.standard_b64decode(secret_key)
+        msg = bytearray(pool_id.split('_')[1], 'utf-8') + bytearray(user_id_for_srp, 'utf-8') + \
+            bytearray(secret_key_bytes) + bytearray(time_stamp, 'utf-8')
+        hkdf = aws.get_password_authentication_key(user_id_for_srp,password,srp_b, salt)
+        hmac_obj = hmac.new(hkdf, msg, digestmod=hashlib.sha256)
+        signature_string = (base64.standard_b64encode(hmac_obj.digest())).decode()
+        self.url = "https://cognito-idp.us-east-1.amazonaws.com/"
+        self.headers={
+        "content-type" : "application/x-amz-json-1.1",
+        "x-amz-target": "AWSCognitoIdentityProviderService.RespondToAuthChallenge"
+        }
+        payload ={
+            "ChallengeName":"PASSWORD_VERIFIER",
+            "ClientId":client_id,
+            "ChallengeResponses":{
+            "USERNAME":user_name,
+            "PASSWORD_CLAIM_SECRET_BLOCK":secret_key,
+            "TIMESTAMP":time_stamp,
+            "PASSWORD_CLAIM_SIGNATURE":signature_string
+            }
+            }
+        print(f"username: {username}")
+        self.payload=json.dumps(payload)
+
+        res2 = requests.post(url=self.url, headers=self.headers, data=self.payload)
+        print('call 2:',res2.status_code)
+        return res2.json()['AuthenticationResult']['AccessToken']
 
 
 if __name__=='__main__':
-    try:
-        x=egoclient()
-        access_token=x.login_with_srp(username="vltest1@gmail.com",password="Test@1234")
-        t = input('Enter <item> for item category or <sub> for subcriptions:')
-        if t=='item':
-            x.item_category()
-        elif t=='sub':
-            x.subcription(access_token)
-        else:
-            raise Exception('incorrect method entered, please enter <item> for item category or <sub> for subcriptions.')
-    except Exception as e:
-        print('Error is :',e)
+    # try:
+    x=egoclient()
+    access_token=x.user_login_with_srp(username="+918309665409",password="Asdf@123")
+    print(access_token)
+    t = input('Enter <item> for item category or <sub> for subcriptions:')
+    if t=='item':
+        x.item_category()
+    elif t=='sub':
+        x.subcription(access_token)
+    else:
+        raise Exception('incorrect method entered, please enter <item> for item category or <sub> for subcriptions.')
+    # except Exception as e:
+    #     print('Error is :',e)
